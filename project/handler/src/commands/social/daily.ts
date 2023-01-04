f7973c2b5184b24d44a423380801acbc3a89da16
Alexis Vielma <alexis.vielma.us@gmail.com>
2023-01-03 19:13:56 -0600

    feat(handler): add social commands

---
+++ project/handler/src/commands/social/daily.ts
@@ -0,0 +1,59 @@
+/**
+ * This file is part of the Rice Ball (https://github.com/aelxxs/riceball).
+ * Copyright (c) 2023 Alexis Vielma.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+import type { Command, Context } from "@lib/core";
+import { getGuild, getMember, updateMember } from "db";
+import ms from "ms";
+
+const ONE_DAY = 86400000;
+
+export default class implements Command {
+	/**
+	 * Claim your daily reward
+	 *
+	 * @param {Context} context - The context of the command
+	 **/
+	public async chatInputRun({ user, guild }: Context) {
+		const { lastDaily } = await getMember(user.id, guild.id);
+
+		const now = Date.now();
+		const diff = now - lastDaily;
+
+		if (diff >= ONE_DAY) {
+			const { economy } = await getGuild(guild.id);
+			const { dailyRewardMin, dailyRewardMax } = economy;
+
+			const reward = Math.floor(Math.random() * (dailyRewardMax - dailyRewardMin + 1)) + dailyRewardMin;
+
+			const { bal } = await getMember(user.id, guild.id);
+
+			await updateMember(user.id, guild.id, {
+				bal: bal + reward,
+				lastDaily: now,
+			});
+
+			return `You claimed your daily reward of \`${reward.toLocaleString()}\` ${
+				economy.currencyIcon
+			}! You now have a balance of \`${(bal + reward).toLocaleString()}\` ${economy.currencyIcon}`;
+		}
+
+		const timeLeft = ONE_DAY - diff;
+
+		return `You can claim your daily reward in \`${ms(timeLeft, { long: true })}\`!`;
+	}
+}
---
+++ project/handler/src/commands/social/reputation.ts
@@ -0,0 +1,67 @@
+/**
+ * This file is part of the Rice Ball (https://github.com/aelxxs/riceball).
+ * Copyright (c) 2023 Alexis Vielma.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+import type { Command, Context } from "@lib/core";
+import { getUser, updateUser } from "db";
+import type { APIUser } from "discord-api-types/v10";
+import ms from "ms";
+
+const ONE_DAY = 86400000;
+
+export default class implements Command {
+	/**
+	 * Give a user reputation
+	 *
+	 * @param {Context} context - The context of the command
+	 * @param {Options} options - The options of the command
+	 **/
+	public async chatInputRun({ guild, user: member, i }: Context, { user }: Options) {
+		if (user.id === member.id) {
+			throw "You can't give yourself reputation!";
+		}
+
+		const { lastReputation } = await getUser(member.id);
+
+		const now = Date.now();
+		const timeDiff = now - (lastReputation ?? 0);
+
+		if (!lastReputation || timeDiff >= ONE_DAY) {
+			const { reputation } = await getUser(user.id);
+
+			await updateUser(user.id, {
+				// TODO: Use Prisma's increment method instead [01/03/2023 @ 19:00]
+				reputation: reputation + 1,
+			});
+
+			await updateUser(member.id, {
+				lastReputation: now,
+			});
+
+			return `You have given <@${user.id}> a reputation point!`;
+		}
+
+		const timeLeft = ms(ONE_DAY - timeDiff, { long: true });
+
+		throw `You must wait **${timeLeft}** in order to give another reputation.`;
+	}
+}
+
+interface Options {
+	/* A user in this server */
+	user: APIUser;
+}
