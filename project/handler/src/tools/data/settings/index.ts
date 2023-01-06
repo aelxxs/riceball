0ae94e529b7687a0760bd882e4d2ca3b6e426893
Alexis Vielma <alexis.vielma.us@gmail.com>
2023-01-06 15:43:30 -0600

    feat(handler): remove tools

--- project/handler/src/tools/data/settings/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./base/SettingsCache";
-export * from "./models/GuildSettingsCache";
-export * from "./models/MemberSettingsCache";
-export * from "./models/UserSettingsCache";
-export * from "./CacheManager";
