a0de9eab0ecdb52bbc699a0d8dd20748208c390a
Alexis Vielma <alexis.vielma.us@gmail.com>
2025-03-31 10:03:46 -0500

    style: format file

--- project/website/svelte.config.js
+++ project/website/svelte.config.js
@@ -2,13 +2,12 @@ import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    files: {
-      lib: "src/library",
-    },
-  },
-
+	kit: {
+		adapter: adapter(),
+		files: {
+			lib: "src/library",
+		},
+	},
 };
 
 export default config;
