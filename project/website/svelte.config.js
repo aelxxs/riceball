3a9ceaf10cc6a03b74620a1e4041daa5f166675e
Alexis Vielma <alexis.vielma.us@gmail.com>
2025-03-31 08:02:53 -0500

    style: format and lint files with biome config

--- project/website/svelte.config.js
+++ project/website/svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    files: {
-      lib: "src/library",
-    },
-  },
+	kit: {
+		adapter: adapter(),
+		files: {
+			lib: "src/library",
+		},
+	},
 };
 
 export default config;
