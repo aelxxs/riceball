09b9953b0c5f7855ff349d148f5db41ddf0d3729
Alexis Vielma <alexis.vielma.us@gmail.com>
2025-04-24 07:42:43 -0500

    feat: add link previews to sidebar

--- project/website/src/library/layout/dashboard/components/sidebar.svelte
+++ project/website/src/library/layout/dashboard/components/sidebar.svelte
@@ -10,7 +10,8 @@ import { type SidebarRoute, generateSidebarRoutes } from "$lib/constants";
 import type { DashboardGuild, ManagedGuild } from "$lib/types";
 import { layoutState } from "$lib/utility/context.svelte";
 import { flyAndScale } from "$lib/utility/transitions";
-import { Select } from "bits-ui";
+import { LinkPreview, Select, Tooltip } from "bits-ui";
+import { fly } from "svelte/transition";
 
 type Props = {
 	guild: DashboardGuild;
@@ -36,7 +37,7 @@ let open = $state(false);
 <Select.Root type="single" bind:open value={guild.id}>
   <Select.Trigger class="header-trigger repel">
     {#snippet child({ props })}
-      <div {...props}>
+      <div {...props} class:open>
         <div class="cluster">
           <DiscordIcon
             size={1.75}
@@ -90,7 +91,6 @@ let open = $state(false);
               </Select.Item>
             {/each}
             <Divider />
-
             <Select.Item value="add" label="Add a new server">
               {#snippet child({ props })}
                 <a href="/manage" class="repel item" {...props}>
@@ -108,35 +108,77 @@ let open = $state(false);
   </Select.Content>
 </Select.Root>
 
-<nav class="flow">
-  {#each Object.entries(sections) as [name, items]}
-    <div class="stack">
-      <small class="fw-md txt-bold">{name}</small>
-      <div class="stack space-2xs">
-        {#each items as { link, name, icon }}
-          {@const Icon = icon}
-          <a
-            href={link}
-            class="link"
-            class:active={plugin.name === name}
-            data-sveltekit-preload-data
-          >
-            <div class="cluster space-xs">
-              <Icon class="icon" size={18} />
-              <p>{name}</p>
-            </div>
-          </a>
-        {/each}
+<Tooltip.Provider delayDuration={100} ignoreNonKeyboardFocus>
+  <nav class="flow">
+    {#each Object.entries(sections) as [name, items]}
+      <div class="stack">
+        <small class="fw-md txt-bold">{name}</small>
+        <div class="stack space-2xs">
+          {#each items as { link, name, icon, extendedInfo }}
+            {@const Icon = icon}
+            <Tooltip.Root>
+              <Tooltip.Trigger>
+                {#snippet child({ props })}
+                  <a
+                    href={link}
+                    class="link"
+                    class:active={plugin.name === name}
+                    data-sveltekit-preload-data="hover"
+                    {...props}
+                  >
+                    <div class="cluster space-xs">
+                      <Icon class="icon" size={18} />
+                      <p>{name}</p>
+                    </div>
+                  </a>
+                {/snippet}
+              </Tooltip.Trigger>
+              <Tooltip.Content
+                alignOffset={-8}
+                sideOffset={8}
+                side="right"
+                forceMount
+              >
+                {#snippet child({ wrapperProps, props, open })}
+                  {#if open}
+                    <div {...wrapperProps}>
+                      <div
+                        class="box stack extended-info-content"
+                        {...props}
+                        transition:flyAndScale
+                      >
+                        <div class="cluster">
+                          <div class="cluster space-xs">
+                            <Icon class="icon" size={18} />
+                            <p class="fw:md">{extendedInfo.title}</p>
+                          </div>
+                        </div>
+                        <p>{extendedInfo.description}</p>
+                      </div>
+                    </div>
+                  {/if}
+                {/snippet}
+              </Tooltip.Content>
+            </Tooltip.Root>
+          {/each}
+        </div>
       </div>
-    </div>
-  {/each}
-</nav>
+    {/each}
+  </nav>
+</Tooltip.Provider>
 
 <style>
   nav {
     padding: var(--space-m);
     overflow-y: auto;
     height: calc(100% - var(--header-height));
+  }
+
+  .extended-info-content {
+    --bg: var(--clr-bg-accent);
+    border: 1px solid var(--clr-bg-border);
+    max-width: 45ch;
+    z-index: 2;
   }
 
   .header-trigger {
@@ -162,16 +204,18 @@ let open = $state(false);
     --border-radius: 0;
     --bg: var(--clr-bg-accent);
     --padding: var(--space-xs);
+    backdrop-filter: blur(5rem);
     width: calc(var(--bits-select-anchor-width));
     border-bottom: 1px solid var(--clr-bg-border);
     z-index: 1;
   }
 
+  .header-trigger.open,
   .header-trigger:hover,
   .header-trigger:focus,
   .header-trigger:focus-visible,
   .header-trigger:active {
-    background-color: var(--clr-bg-translucent);
+    background-color: var(--clr-bg-accent);
   }
 
   .chevron {
